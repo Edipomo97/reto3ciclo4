7914adde3c84e291674903b2969fe7402474f6de
Edipomo97 <59237405+Edipomo97@users.noreply.github.com>
2022-12-02 23:00:14 -0500

    cambio de enlace en databaseURL

--- lib/config/configuration.dart
+++ lib/config/configuration.dart
@@ -2,13 +2,17 @@
 // copy the values from firebase
 
 class Configuration {
-  static const apiKey = "AIzaSyBEeShMQN1Nqvl9aPk7kLxKvO2Sr4g-Og0";  // tomado de current_key
-  static const authDomain = "983664459314-hi3202f2kkf1oms7gkmkhlk5v1moubip.apps.googleusercontent.com"; // tomado de oauth_client
+  static const apiKey =
+      "AIzaSyBEeShMQN1Nqvl9aPk7kLxKvO2Sr4g-Og0"; // tomado de current_key
+  static const authDomain =
+      "983664459314-hi3202f2kkf1oms7gkmkhlk5v1moubip.apps.googleusercontent.com"; // tomado de oauth_client
   static const databaseURL =
-      "https://console.firebase.google.com/project/appmensajesg1/database/appmensajesg1-default-rtdb/data/~2F"; // tomado de firebase_url
+      "https://appmensajesg1-default-rtdb.firebaseio.com"; // tomado de firebase_url
   static const projectId = "appmensajesg1"; // tomado de project_id
-  static const storageBucket = "appmensajesg1.appspot.com"; // tomado de storage_bucket
+  static const storageBucket =
+      "appmensajesg1.appspot.com"; // tomado de storage_bucket
   static const messagingSenderId = ""; //no lo vamos a usar
-  static const appId = "1:983664459314:android:604f2fb675638f44a3462b"; // tomado de mobilesdk_app_id
+  static const appId =
+      "1:983664459314:android:604f2fb675638f44a3462b"; // tomado de mobilesdk_app_id
   static const measurementId = ""; //no lo vamos a usar
 }
